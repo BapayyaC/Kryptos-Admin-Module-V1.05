9027a22bae088b3f9b9e8415a5878cad688a8d54
BapayyaC <bapayyac@gmail.com>
2017-05-10 21:42:17 +0530

    Kryptos-Admin-Module-V1.05
    Release v1.05

---
+++ Kryptos/Controllers/OrganisationAdminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kryptos.Controllers
+{
+    public class OrganisationAdminController : Controller
+    {
+        //
+        // GET: /OrganisationAdmin/
+
+        public ActionResult NewUser()
+        {
+            ViewData["current view"] = "Organization List";
+            if (LoginController.ActiveUser != null)
+            {
+                ViewData["CURRENTUSER"] = LoginController.ActiveUser;
+            }
+            else
+            {
+                TempData["errormsg"] = "Please Login and Proceed!";
+                return RedirectToAction("Login", "Login");
+            }
+
+            return View();
+        }
+
+       
+
+    }
+}
